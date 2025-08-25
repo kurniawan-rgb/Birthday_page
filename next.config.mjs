c31604ebcc26de3ceb60a7f785d0f203a920dd15
“kurniawan-rgb” <71911982+kurniawan-rgb@users.noreply.github.com>
2025-08-25 14:06:11 +0800

    upadte

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,7 @@ const nextConfig = {
   // PENTING: Ganti 'nama-repo-anda' dengan nama repository GitHub Anda!
   // Contoh: jika repo Anda adalah https://github.com/username/ultah-pacar
   // maka isinya adalah '/ultah-pacar'
-  basePath: "/nama-repo-anda",
-
+  basePath: "https://kurniawan-rgb.github.io/Birthday_page",
   // Diperlukan untuk Next.js Image component agar bekerja di GitHub Pages
   images: {
     unoptimized: true,
